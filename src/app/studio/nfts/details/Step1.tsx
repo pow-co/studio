50d7ac4187ec141705165d3352b01557a39bb565
Guillaume De Martino <guillaumedemartino@gmail.com>
2023-06-28 16:40:47 +0200

    fix smol typo

--- src/app/studio/nfts/details/Step1.tsx
+++ src/app/studio/nfts/details/Step1.tsx
@@ -150,7 +150,7 @@ const Step1 = ({
                     <label htmlFor="dropzone" className="flex min-h-[27.5rem] w-full cursor-pointer flex-col items-center justify-center rounded-lg hover:bg-stone-800 ">
                         <div className="flex flex-col items-center justify-center pb-6 pt-5">
                             <svg aria-hidden="true" className="mb-3 h-8 w-8 text-stone-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
+                                <path strokeLinecap="round" stroke-linejoin="round" stroke-width="1" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12" />
                             </svg>
                             <p className="mb-2">
                             Click to add your NFT media or drag-and-drop it here
